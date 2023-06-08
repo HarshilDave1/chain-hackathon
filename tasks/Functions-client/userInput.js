60671ab406655134f1f2ff9a926dd152273b75c1
HarshilDave1 <harshil128@gmail.com>
2023-06-08 02:17:44 -0400

    update userInput.js

--- tasks/Functions-client/userInput.js
+++ tasks/Functions-client/userInput.js
@@ -3,53 +3,37 @@ const promptin = require("prompt");
 
 // Load the configuration file
 const configPath = "../../Functions-request-config.js";
+const savePath = "Functions-request-config-new.js";
 let requestConfig = require(configPath);
 
 task("userInput", "Update the args field in the configuration")
-    .setAction(async () => {
-        // Create a prompt schema
-        const schema = {
-            properties: {
-                userInput: {
-                    description: "Enter your text prompt:",
-                    type: "string",
-                    required: true
-                }
-            }
-        };
+    .addParam("newvalue", "The new value for args")
+    .setAction(async (taskArgs) => {
+        // Access the value passed as the "newvalue" parameter
+        const newValue = taskArgs["newvalue"];
 
-        // Start the prompt to get user input
-        promptin.start();
+        // Update the args field in the configuration
+        requestConfig.args = [newValue];
 
-        // Get the user input
-        promptin.get(schema, (err, result) => {
-            if (err) {
-                console.error("Error while capturing user input:", err);
-                return;
-            }
+        // Set the sensitive values from environment variables
+        requestConfig.walletPrivateKey = process.env.PRIVATE_KEY; //Is this a security issue?
 
-            // Access the user input
-            const userInput = result.userInput;
+        requestConfig.secrets = { openaiKey: process.env.OPENAI_KEY };
 
-            // Update the args field in the configuration
-            requestConfig.args = [userInput];
+        // Generate the modified configuration as a string
+        const configContent = `module.exports = ${JSON.stringify(requestConfig, null, 2)};\n`;
 
-            // Generate the modified configuration as a string
-            const configContent = `module.exports = ${JSON.stringify(requestConfig, null, 2)};\n`;
+        // Save the modified configuration to the file
+        fs.writeFileSync(savePath, configContent);
 
-            // Save the modified configuration to the file
-            fs.writeFileSync(configPath, configContent);
+        console.log("Args field updated:", requestConfig.args);
+        console.log("Configuration file saved at", savePath);
 
-            console.log("Args field updated:", requestConfig.args);
-            console.log("Configuration file saved.");
+        // Clear the module cache to force a fresh load of the modified file
+        delete require.cache[require.resolve(configPath)];
 
-            // Clear the module cache to force a fresh load of the modified file
-            delete require.cache[require.resolve(configPath)];
+        // Reload the modified configuration
+        requestConfig = eval(configContent);
 
-            // Reload the modified configuration
-            requestConfig = eval(configContent);
-
-            // Perform any other necessary actions
-        });
+        // Perform any other necessary actions
     });
-
